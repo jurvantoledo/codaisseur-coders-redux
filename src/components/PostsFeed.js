0666b5846ad44ea375306f6d10546fdf0563688d
jurvantoledo <jurvantoledo@live.nl>
2020-11-04 16:16:53 +0100

    modified in redux state

--- src/components/PostsFeed.js
+++ src/components/PostsFeed.js
@@ -1,30 +1,29 @@
 // src/component/PostFeed.js
 
-import React, { useEffect, useState } from "react";
+import React, { useEffect } from "react";
 import axios from "axios";
 import moment from "moment";
+import { useDispatch, useSelector } from "react-redux";
+import { startLoading, postsFetched } from "../store/feed/actions";
+import { selectFeedLoading, selectFeedPosts } from "../store/feed/selector";
 
 const API_URL = `https://codaisseur-coders-network.herokuapp.com`;
 
 export default function PostsFeed() {
-  const [data, setData] = useState({
-    loading: false,
-    posts: [],
-  });
+  const dispatch = useDispatch();
+
+  const loading = useSelector(selectFeedLoading);
+  const posts = useSelector(selectFeedPosts);
 
   async function fetchNext5Posts() {
-    setData({ ...data, loading: true });
-
+    dispatch(startLoading);
     const response = await axios.get(
-      `${API_URL}/posts?offset=${data.posts.length}&limit=5`
+      `${API_URL}/posts?offset=${posts.length}&limit=5`
     );
 
     const morePosts = response.data.rows;
 
-    setData({
-      loading: false,
-      posts: [...data.posts, ...morePosts],
-    });
+    dispatch(postsFetched(morePosts));
     console.log(morePosts);
   }
 
@@ -35,7 +34,7 @@ export default function PostsFeed() {
   return (
     <div className="PostsFeed">
       <h2>Recent posts</h2>
-      {data.posts.map((post) => {
+      {posts.map((post) => {
         return (
           <div key={post.id}>
             <h3>{post.title}</h3>
@@ -46,6 +45,8 @@ export default function PostsFeed() {
           </div>
         );
       })}
+      {loading}
+      <em>loading...</em>
       <button onClick={fetchNext5Posts}>Load more</button>
     </div>
   );
--- src/store/reducer.js
+++ src/store/reducer.js
@@ -1,9 +1,10 @@
 // src/store/reducer.js
 import { combineReducers } from "redux";
 import someFeatureReducer from "./someFeature/reducer";
+import feedSliceReducer from "./feed/reducer";
 
 const reducer = combineReducers({
-  someFeature: someFeatureReducer,
+  feed: feedSliceReducer,
 });
 
 export default reducer;
